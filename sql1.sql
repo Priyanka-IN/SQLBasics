e6744f90d0ea85edb78a42137b5e6a6ca6a0f45d
Priyanka-IN <adsul.priya@gmail.com>
2020-06-17 01:24:17 +0530

    Update sql1.sql
    Problem 2

--- sql1.sql
+++ sql1.sql
@@ -1,3 +1,5 @@
+
+--Problem 1 : floor for rounding off
 select  country.continent, floor(AVG(city.population)) as population
 from country, city where city.countrycode = country.code
 group by country.continent; 
@@ -6,4 +8,9 @@ select  country.continent, ROUND(AVG(city.population)-0.5,0) as population
 from country, city where city.countrycode = country.code
 group by country.continent;  
 
-Thi is test push 
+--Problem 2 : MySQL
+
+select if(A+B<=C or B+C<=A or A+C<=B,'Not A Triangle',
+if(A=B and B=C,'Equilateral',
+if(A=B or B=C or A=C,'Isosceles','Scalene'))) 
+from TRIANGLES as T;
