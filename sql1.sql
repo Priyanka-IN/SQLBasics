d4d0142a6d31850b662a2c383a69f07ff7656401
Priyanka-IN <adsul.priya@gmail.com>
2020-06-17 02:07:40 +0530

    Update sql1.sql
    mysql functions

--- sql1.sql
+++ sql1.sql
@@ -14,3 +14,17 @@ select if(A+B<=C or B+C<=A or A+C<=B,'Not A Triangle',
 if(A=B and B=C,'Equilateral',
 if(A=B or B=C or A=C,'Isosceles','Scalene'))) 
 from TRIANGLES as T;
+
+
+--Problem 3 : slect with mysql functions
+
+select 
+CONCAT(name, "(", SUBSTR(occupation, 1, 1), ")") as newname
+from occupations
+group by newname;
+
+
+select CONCAT("There are a total of ", count(occupation)," ",LOWER(occupation),"s.")
+from occupations
+group by occupation
+order by count(occupation), occupation;
